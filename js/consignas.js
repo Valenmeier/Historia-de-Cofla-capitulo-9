5acdaf38dc94dfb61eff086a236858201dc98b84
Valentin Meier <valentinmeier123@gmail.com>
2022-07-23 22:44:19 -0300

    Primer problema

---
+++ js/consignas.js
@@ -0,0 +1,5 @@
+//Todo: Cofla reprobo dos materias , debido a esto va a tener que enviar un formulario a las mesas de examenes, para poder entregarlas
+//?PROBLEMA A
+//* -El formulario debe tener nombre completo, mail y materia adeudada
+//* -Se debe validar que el nombre sea valido y que los nombres sean reales
+//* -Se debe enviar al servidor de manera pulida
---
+++ js/crearSoluciones1.js
@@ -0,0 +1,40 @@
+const nombre=document.querySelector(`#nombre`);
+const email=document.querySelector(`#email`);
+const materia=document.querySelector(`#materia`);
+const enviar=document.querySelector(`#btn-enviar`);
+const resultado=document.querySelector(`.resultado`)
+
+
+enviar.addEventListener(`click`, (e)=> {  
+    e.preventDefault()
+    let error=validarCampos();
+    if (error[0]) {
+        resultado.innerHTML=error[1]
+        resultado.classList.add(`red`)
+    }else {
+        resultado.innerHTML=`Solicitud enviada correctamente`;
+        resultado.classList.add(`green`)
+        resultado.classList.remove(`red`)
+    }
+})
+
+const validarCampos= ()=> {
+    let error= [];
+    if (nombre.value.length<5|| nombre.value.length>40) {
+        error[0]=true
+        error[1]=`El nombre es inválido`
+        return error;
+    }else if (  email.value.length<5 ||
+                email.value.length > 40 ||
+                email.value.indexOf(`@`) == -1 ||
+                email.value.indexOf(`.`)== -1){
+        error[0]=true
+        error[1]=`El mail es inválido`
+        return error
+    }else if (materia.value<4 || materia.value>40) {
+        error[0]=true
+        error[1]=`La materia no existe`
+    }
+    error[0]=false
+    return error
+}
